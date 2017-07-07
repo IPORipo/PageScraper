f1e09383e3af09f3c1c9fbd0e61c2586b40265a6
ripo <roma.simbadze@gmail.com>
2017-07-08 00:13:08 +0400

    Deleted extra comments

--- src/controller/Home.java
+++ src/controller/Home.java
@@ -16,11 +16,6 @@ import org.jsoup.select.Elements;
 
 import services.ScraperFunctions;
 
-//import org.w3c.dom.Element;
-
-/**
- * Servlet implementation class simpleparser
- */
 @WebServlet("/home")
 public class Home extends HttpServlet {
 	private static final long serialVersionUID = 1L;
